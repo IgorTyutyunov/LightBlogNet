32f99233e02582a887ee8abcf109c1218a67b77c
Igor Tyutyunov <igrik5181@gmail.com>
2019-07-20 23:23:25 +0300

    Готовый блог

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'welcom/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcom#index'
+  resources :articles
+  resources :articles do
+  resources :comments
+end
+end
